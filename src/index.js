e43a1b42abcc2279219c79b7973914ee47b36fce
chrissiku <chrissiku5@gmail.com>
2022-06-01 11:52:25 +0200

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,45 +1,45 @@
-import "./style.css";
-import Image from "./bg2.jpg";
-const listContent = document.querySelector("#list");
+import './style.css';
+
+const listContent = document.querySelector('#list');
 const tasks = [
   {
     id: 1,
-    description: "Hello, this is my task 1",
+    description: 'Hello, this is my task 1',
     completed: true,
   },
   {
     id: 2,
-    description: "Hello, this is my task 2",
+    description: 'Hello, this is my task 2',
     completed: false,
   },
   {
     id: 3,
-    description: "Hello, this is my task 3",
+    description: 'Hello, this is my task 3',
     completed: true,
   },
   {
     id: 4,
-    description: "Hello, this is my task 4",
+    description: 'Hello, this is my task 4',
     completed: false,
   },
   {
     id: 5,
-    description: "Hello, this is my task 5",
+    description: 'Hello, this is my task 5',
     completed: true,
   },
   {
     id: 6,
-    description: "Hello, this is my task 6",
+    description: 'Hello, this is my task 6',
     completed: false,
   },
   {
     id: 7,
-    description: "Hello, this is my task 7",
+    description: 'Hello, this is my task 7',
     completed: false,
   },
   {
     id: 8,
-    description: "Hello, this is my task 8",
+    description: 'Hello, this is my task 8',
     completed: false,
   },
 ].reverse();
@@ -47,9 +47,9 @@ const tasks = [
 const displayList = () => {
   for (let i = 0; i < tasks.length; i += 1) {
     const myLoop = listContent.innerHTML;
-    let state = "";
+    let state = '';
     if (tasks[i].completed === true) {
-      state = "checked";
+      state = 'checked';
     }
     listContent.innerHTML = `
               <li>
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    index: "./src/index.js",
+    index: './src/index.js',
   },
   devServer: {
-    static: "./docs",
+    static: './docs',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "docs"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
   optimization: {
-    runtimeChunk: "single",
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
